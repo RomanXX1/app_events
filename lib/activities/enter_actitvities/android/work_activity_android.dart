70eb6aae3d2af71843ecf7949ae13006927b56fd
RomanXXI <tra-ss@mail.ru>
2021-02-10 16:08:50 +0700

    - релиз "ноль-повдоль"

--- lib/activities/enter_actitvities/android/work_activity_android.dart
+++ lib/activities/enter_actitvities/android/work_activity_android.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:app_events/server/get_data_ver.dart';
+import 'package:app_events/server/repository.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,6 +14,12 @@ class _WorkActivity_AndroidState extends State<WorkActivity_Android> {
   String path = '';
   String login = '';
   String pass = '';
+  bool isLoading = true;
+
+  // Данные для вывода
+  String platform = '';
+  String name = '';
+  String ver = '';
 
   @override
   void initState() {
@@ -22,19 +32,70 @@ class _WorkActivity_AndroidState extends State<WorkActivity_Android> {
     path = prefs.getString('path') ?? '';
     login = prefs.getString('login') ?? '';
     pass = prefs.getString('pass') ?? '';
+    ObjRepository repository =
+        ObjRepository(path_DB: path, login: login, pass: pass);
+
+    repository.get_data().then((response) {
+      setState(() {
+        platform = response.platform;
+        if (response.name == "65915") {
+          name = 'Комплексная автоматизация 2';
+        } else {
+          name = 'Необрабатываемый тип конфигурации';
+        }
+        ver = response.ver;
+        isLoading = false;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Информация')),
-      body: Column(
-        children: [
-          Text(path),
-          Text(login),
-          Text(pass),
-        ],
-      ),
+      body: isLoading ? Center(child: CircularProgressIndicator()) : info(),
+    );
+  }
+
+  Widget info() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: Text(
+              'Вы подключились к информационной базе',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 32.0, left: 16.0),
+          child: Text(
+            'Параметры подключения:',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+          child: Text(
+            '1С:Предприятие ' + platform,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+          child: Text(
+            name,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0, left: 16.0),
+          child: Text(
+            'Релиз ' + ver,
+          ),
+        ),
+      ],
     );
   }
 }
